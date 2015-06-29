1dc81f203a191e410b5fc75b33c1e61b551a0c2d
Joel Purra <code+github@joelpurra.com>
2015-06-29 18:49:15 +0200

    POC file upload directly to S3

--- app/web.js
+++ app/web.js
@@ -2,6 +2,10 @@
 
 var configuration = require("configvention"),
 
+    AWS_ACCESS_KEY = configuration.get("AWS_ACCESS_KEY"),
+    AWS_SECRET_KEY = configuration.get("AWS_SECRET_KEY"),
+    S3_BUCKET = configuration.get("S3_BUCKET"),
+
     bunyanConfig = {
         name: "ftrf-web",
 
@@ -80,6 +84,8 @@ var configuration = require("configvention"),
         index: "index.html"
     }),
 
+    aws = require("aws-sdk"),
+
     app = express();
 
 app.use(expressLogger);
@@ -93,10 +99,69 @@ app.use(helmet.hsts({
 
 app.use(configuredHttpsRedirect());
 
+(function() {
+    // Based on https://github.com/flyingsparx/NodeDirectUploader
+    // Apache 2.0 license.
+    // By https://github.com/flyingsparx/
+    // https://devcenter.heroku.com/articles/s3-upload-node
+
+    /*
+     * Respond to GET requests to /sign_s3.
+     * Upon request, return JSON containing the temporarily-signed S3 request and the
+     * anticipated URL of the image.
+     */
+    app.get("/sign_s3", function(req, res) {
+        aws.config.update({
+            accessKeyId: AWS_ACCESS_KEY,
+            secretAccessKey: AWS_SECRET_KEY
+        });
+        aws.config.update({
+            region: "eu-central-1",
+            signatureVersion: "v4"
+        });
+        var s3 = new aws.S3();
+        var s3_params = {
+            Bucket: S3_BUCKET,
+            Key: req.query.file_name,
+            Expires: 60,
+            ContentType: req.query.file_type,
+            ACL: "public-read"
+        };
+        s3.getSignedUrl("putObject", s3_params, function(err, data) {
+            if (err) {
+                logger.error(err);
+            } else {
+                var generatedFilename = (new Date().valueOf() + req.query.file_name),
+                    return_data = {
+                        signed_request: data,
+                        url: "https://" + S3_BUCKET + ".s3.amazonaws.com/" + "before/" + generatedFilename
+                    };
+                res.write(JSON.stringify(return_data));
+                res.end();
+            }
+        });
+    });
+
+    /*
+     * Respond to POST requests to /submit_form.
+     * This function needs to be completed to handle the information in 
+     * a way that suits your application.
+     */
+    app.post("/submit_form", function(req, res) {
+        username = req.body.username;
+        full_name = req.body.full_name;
+        avatar_url = req.body.avatar_url;
+        update_account(username, full_name, avatar_url); // TODO: create this function
+        // TODO: Return something useful or redirect
+    });
+}());
+
 app.use(mount);
 
 app.listen(httpServerPort, httpServerIp, function() {
     logger.info("Listening on port", httpServerPort);
     logger.info("Bound to interface with ip", httpServerIp);
     logger.info("Serving site root from folder", siteRootPath);
+    logger.info("Using AWS_ACCESS_KEY", AWS_ACCESS_KEY);
+    logger.info("Using S3_BUCKET", S3_BUCKET);
 });
--- public/resources/javascript/main.js
+++ public/resources/javascript/main.js
@@ -1,2 +1,68 @@
-function noop() {
-};
+(function() {
+    // Based on https://github.com/flyingsparx/NodeDirectUploader
+    // Apache 2.0 license.
+    // By https://github.com/flyingsparx/
+    // https://devcenter.heroku.com/articles/s3-upload-node
+
+    /*
+        Function to carry out the actual PUT request to S3 using the signed request from the app.
+    */
+    function upload_file(file, signed_request, url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("PUT", signed_request);
+        xhr.setRequestHeader("x-amz-acl", "public-read");
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                document.getElementById("preview").src = url;
+                document.getElementById("avatar_url").value = url;
+            }
+        };
+        xhr.onerror = function() {
+            alert("Could not upload file.");
+        };
+        xhr.send(file);
+    }
+
+    /*
+        Function to get the temporary signed request from the app.
+        If request successful, continue to upload the file using this signed
+        request.
+    */
+    function get_signed_request(file) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "/sign_s3?file_name=" + file.name + "&file_type=" + file.type);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    var response = JSON.parse(xhr.responseText);
+                    upload_file(file, response.signed_request, response.url);
+                } else {
+                    alert("Could not get signed URL.");
+                }
+            }
+        };
+        xhr.send();
+    }
+
+    /*
+       Function called when file input updated. If there is a file selected, then
+       start upload procedure by asking for a signed request from the app.
+    */
+    function init_upload() {
+        console.log("here");
+        var files = document.getElementById("file_input").files;
+        var file = files[0];
+        if (file == null) {
+            alert("No file selected.");
+            return;
+        }
+        get_signed_request(file);
+    }
+
+    /*
+       Bind listeners when the page loads.
+    */
+    (function() {
+        document.getElementById("file_input").onchange = init_upload;
+    })();
+})();
